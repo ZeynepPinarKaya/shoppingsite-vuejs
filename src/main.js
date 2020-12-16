6705c74415ac9fef182e64e703d25c6aeedee317
zeyneppinar <zeynepinarkaya@gmail.com>
2020-12-16 22:14:01 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import "animate.css/animate.compat.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign);
 
 Vue.config.productionTip = false;
 
